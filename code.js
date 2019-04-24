e8347cb446847f11c3fbecb3b5c76354ae7e69c1
Andreas Valsamis <andval@protonmail.com>
2019-04-24 13:05:23 +0200

    added xhr(), corrected some HTML

--- code.js
+++ code.js
@@ -1,7 +1,22 @@
+window.onload=function(){
 // declare variables
-var url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+var url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 var xhr = document.querySelector("#Xhr");
 var fetch = document.querySelector("#Fetch");
 var jquerry  = document.querySelector("#Jquerry");
 var axios = document.querySelector("#Axios");
-var quotes = document.querySelector("#Quotes");
+var quote = document.querySelector("#Quotes");
+
+// XHR 
+xhr.addEventListener("click",function(){
+    var xhr2 = new XMLHttpRequest();
+    xhr2.onreadystatechange=function(){
+      console.log(xhr2.responseText);
+      quote.innerText=JSON.parse(xhr2.responseText);
+    }
+    xhr2.open("GET",url);
+    xhr2.send();
+      });
+
+
+}
