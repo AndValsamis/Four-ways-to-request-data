55c805dd8ec5aa54fe05745a0872059173189f85
Andreas Valsamis <andval@protonmail.com>
2019-04-24 12:31:13 +0200

    create code.js, adding libraries to HTML,creating variables of js

---
+++ code.js
@@ -0,0 +1,7 @@
+// declare variables
+var url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+var xhr = document.querySelector("#Xhr");
+var fetch = document.querySelector("#Fetch");
+var jquerry  = document.querySelector("#Jquerry");
+var axios = document.querySelector("#Axios");
+var quotes = document.querySelector("#Quotes");
