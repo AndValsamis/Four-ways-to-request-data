1baa1bcbed9b3177e562aca4cd0d477b29763e87
Andreas Valsamis <andval@protonmail.com>
2019-04-24 15:08:28 +0200

    added fetch and axios, updated some HTML

--- code.js
+++ code.js
@@ -2,9 +2,9 @@ window.onload=function(){
 // declare variables
 var url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 var xhr = document.querySelector("#Xhr");
-var fetch = document.querySelector("#Fetch");
+var fetch2 = document.querySelector("#Fetch");
 var jquerry  = document.querySelector("#Jquerry");
-var axios = document.querySelector("#Axios");
+var axios2 = document.querySelector("#Axios");
 var quote = document.querySelector("#Quote");
 
 // XHR 
@@ -12,13 +12,24 @@ xhr.addEventListener("click",function(){
     var xhr2 = new XMLHttpRequest();
     xhr2.onreadystatechange=function(){
       console.log(xhr2.responseText);
-      quote.innerText=JSON.parse(xhr2.responseText);
+      quote.innerText=JSON.parse(xhr2.responseText);s
     }
     xhr2.open("GET",url);
     xhr2.send();
       });
 
 // Fetch
+fetch2.addEventListener("click",function(){
+    fetch(url)
+    .then(function(response){
+      console.log(response);
+      return response.json();
+    })
+    .then(function(response){
+      console.log(response);
+      quote.innerText=response[0];
+    })
+  });
 
 // JQuerry
 $('#Jquerry').click(function(){
@@ -28,6 +39,18 @@ $('#Jquerry').click(function(){
       $("#Quote").text(response[0]);
     })
   });
-}
+
 
 // Axios
+axios2.addEventListener("click",function(){
+    axios(url)
+      .then(function(response){
+      console.log(response);
+      quote.innerText=response.data[0];
+      
+   })
+  });
+
+
+
+}//onload
